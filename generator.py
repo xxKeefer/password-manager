9d3bfb7a98e1be89842d96a0e4eae8a11d41a3e5
xxKeefer <xxkeefer.code@gmail.com>
2024-09-19 00:28:07 +1000

    docs: :memo: add comments to code

--- generator.py
+++ generator.py
@@ -7,15 +7,22 @@ class Generator:
     def __init__(self, length, categories):
         self.length = length
         self.categories = categories
+        # rounding this up so we get more instead of less characters
+        # in the case of a fraction, and then cut characters later
         self.per_category = math.ceil(length / len(categories))
 
     def generate(self):
+        # create a list to add the selected characters too
         output = []
 
+        # loop through each category the user selected
         for choice in self.categories:
             if choice == 'Uppercase':
+                # create a list of characters for the category
                 uppers = list(string.ascii_uppercase)
+                # select a random subset of the character
                 uppers_selected = random.sample(uppers, self.per_category)
+                # add them to the list
                 output.extend(uppers_selected)
 
             if choice == 'Lowercase':
@@ -34,6 +41,9 @@ class Generator:
                     symbols, self.per_category)
                 output.extend(symbols_selected)
 
+        # shuffle the selected characters so the categories aren't all lined up
         random.shuffle(output)
-
+        # trim the list to the desired length "[0:self.length]"
+        # this cuts any extra characters because of rounding up self.per_category
+        # join the trimmed list into a string and return
         return "".join(output[0:self.length])
--- main.py
+++ main.py
@@ -3,20 +3,27 @@ import typer
 import inquirer
 from generator import Generator
 
+# validation for the length question, current is the answer the user gave in the terminal
+
 
 def validate_length(_, current):
     if int(current) < 8:
+      # inquirer.errors.ValidationError is a pretty error that doesn't crash the program
         raise inquirer.errors.ValidationError(
             "Too short", reason="Your password will be too simple")
     elif int(current) > 26:
+      # inquirer.errors.ValidationError is a pretty error that doesn't crash the program
         raise inquirer.errors.ValidationError(
             "Too long", reason="Your password may be rejected for being to long in some cases")
 
     return True
 
+# validation for the categories question, current is the answer the user gave in the terminal
+
 
 def validate_categories(_, current):
     if len(current) < 1:
+      # inquirer.errors.ValidationError is a pretty error that doesn't crash the program
         raise inquirer.errors.ValidationError(
             "No Categories", reason="You must select at least one category")
 
@@ -24,21 +31,28 @@ def validate_categories(_, current):
 
 
 def main():
+    # make a list of questions to ask in the terminal
     questions = [
+        # question one
         inquirer.Text(
             "length",
             message="How long should the password be? (8-26 characters)",
+            # this validation function we defined above
             validate=validate_length
         ),
+        # question two
         inquirer.Checkbox(
             "categories",
             message="What types of characters to include in the password?",
             choices=["Uppercase", "Lowercase", "Numbers", "Symbols"],
             default=["Uppercase", "Numbers"],
+            # this validation function we defined above
             validate=validate_categories
         ),
     ]
 
+    # inquirer.prompt iterates of the questions list and asks them in turn
+    # and re asks a question if it's answer does not pass validation
     answers = inquirer.prompt(questions)
     password = Generator(int(answers['length']), answers['categories'])
     print(password.generate())
