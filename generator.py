91ca7769b36402a8bd07b574e922b7971d6eff85
xxKeefer <xxkeefer.code@gmail.com>
2024-09-19 15:50:54 +1000

    feat: :sparkles: add passphrase option

--- generator.py
+++ generator.py
@@ -1,48 +1,72 @@
 import random
 import string
 import math
+from abc import ABC, abstractmethod
 
 
-class Generator:
-    def __init__(self, length, categories):
+class BaseGenerator(ABC):
+    def __init__(self, length):
+        super().__init__()
         self.length = length
+
+    @abstractmethod
+    def generate(self):
+        pass
+
+
+class PasswordGenerator(BaseGenerator):
+    def __init__(self, length, categories):
+        # Call the parent class constructor
+        super().__init__(length)
         self.categories = categories
         # rounding this up so we get more instead of less characters
         # in the case of a fraction, and then cut characters later
         self.per_category = math.ceil(length / len(categories))
+        # create a private dictionary of character lists to pick from
+        self.__char_set = {
+            "Uppercase": list(string.ascii_uppercase),
+            "Lowercase": list(string.ascii_lowercase),
+            "Numbers": list(string.digits),
+            "Symbols": list('!@#$%^&*'),
+        }
+        # create a private list to add the selected characters too
+        self.__characters = []
 
     def generate(self):
-        # create a list to add the selected characters too
-        output = []
-
         # loop through each category the user selected
-        for choice in self.categories:
-            if choice == 'Uppercase':
-                # create a list of characters for the category
-                uppers = list(string.ascii_uppercase)
-                # for the amount of characters from the category we need to add
-                for _ in range(self.per_category):
-                    # add a random one
-                    output.append(random.choice(uppers))
-
-            if choice == 'Lowercase':
-                lowers = list(string.ascii_lowercase)
-                for _ in range(self.per_category):
-                    output.append(random.choice(lowers))
-
-            if choice == 'Numbers':
-                digits = list(string.digits)
-                for _ in range(self.per_category):
-                    output.append(random.choice(digits))
-
-            if choice == 'Symbols':
-                symbols = list("!@#$%^&*")
-                for _ in range(self.per_category):
-                    output.append(random.choice(symbols))
+        for category in self.categories:
+            # for the amount characters per category
+            for _ in range(self.per_category):
+                # add a random character from the set to the character list
+                self.__characters.append(
+                    random.choice(self.__char_set[category]))
 
         # shuffle the selected characters so the categories aren't all lined up
-        random.shuffle(output)
+        random.shuffle(self.__characters)
         # trim the list to the desired length "[0:self.length]"
         # this cuts any extra characters because of rounding up self.per_category
         # join the trimmed list into a string and return
-        return "".join(output[0:self.length])
+        return "".join(self.__characters[0:self.length])
+
+
+class PassphraseGenerator(BaseGenerator):
+    def __init__(self, length, wordlist=None):
+        super().__init__(length)
+        self.__phrase = []
+        # Use a default wordlist if none is provided
+        if wordlist is None:
+            self.wordlist = [
+                "apple", "banana", "cherry", "dog", "elephant", "frog", "giraffe",
+                "house", "ice", "jungle", "kite", "lion", "mountain", "night",
+                "ocean", "penguin", "queen", "river", "sun", "tree", "umbrella",
+                "violet", "whale", "xylophone", "yellow", "zebra"
+            ]
+        else:
+            self.wordlist = wordlist
+
+    def generate(self):
+        # Generate a passphrase by picking random words from the wordlist
+        for _ in range(self.length):
+            self.__phrase.append(random.choice(self.wordlist))
+
+        return "-".join(self.__phrase)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import inquirer.errors
 import typer
 import inquirer
-from generator import Generator
+from generator import PasswordGenerator, PassphraseGenerator
 
 
 # validation for the length question, current is the answer the user gave in the terminal
-def validate_length(_, current):
+def validate_password_length(_, current):
     if int(current) < 5:
       # inquirer.errors.ValidationError is a pretty error that doesn't crash the program
         raise inquirer.errors.ValidationError(
@@ -28,7 +28,18 @@ def validate_categories(_, current):
     return True
 
 
-def main():
+def validate_passphrase_length(_, current):
+    if int(current) < 2:
+        raise inquirer.errors.ValidationError(
+            "Too Few", reason="A passphrase need to be at least two words")
+    elif int(current) > 10:
+        raise inquirer.errors.ValidationError(
+            "Too Many", reason="Let's not go crazy")
+
+    return True
+
+
+def make_password():
     # make a list of questions to ask in the terminal
     questions = [
         # question one
@@ -36,7 +47,7 @@ def main():
             "length",
             message="How long should the password be? (5-128 characters)",
             # this validation function we defined above
-            validate=validate_length
+            validate=validate_password_length
         ),
         # question two
         inquirer.Checkbox(
@@ -52,8 +63,39 @@ def main():
     # inquirer.prompt iterates of the questions list and asks them in turn
     # and re asks a question if it's answer does not pass validation
     answers = inquirer.prompt(questions)
-    password = Generator(int(answers['length']), answers['categories'])
+    password = PasswordGenerator(int(answers['length']), answers['categories'])
     print(password.generate())
+
+
+def make_passphrase():
+    questions = [
+        inquirer.Text(
+            "length",
+            message="How long should the passphrase be? (2-10 words)",
+            validate=validate_passphrase_length
+        ),
+
+    ]
+    answers = inquirer.prompt(questions)
+    passphrase = PassphraseGenerator(int(answers['length']))
+    print(passphrase.generate())
+
+
+def main():
+    kind = [
+        inquirer.List(
+            "kind",
+            message="What kind of password would you like?",
+            choices=["Password", "Passphrase"]
+        ),
+
+    ]
+    choice = inquirer.prompt(kind)
+
+    if choice['kind'] == "Password":
+        make_password()
+    else:
+        make_passphrase()
 
 
 if __name__ == "__main__":
